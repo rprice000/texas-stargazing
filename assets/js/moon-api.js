e2597ea7d8bbdff7f976e76bf480d55c01355ebe
Jsummers1 <julie.summers2000@gmail.com>
2021-10-06 14:38:24 -0500

    replaced API added sunrise/sunset

--- assets/js/moon-api.js
+++ assets/js/moon-api.js
@@ -7,16 +7,22 @@ const n = new Date(d) - 18000000;
 // format date for API to EPOCH
 dt = (n / 1000) | 0;
 
-const url = `http://api.farmsense.net/v1/moonphases/?d[]=1350526582&d[]=1350363600&d[]=1350277200`
+const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/1%20Panther%20Junction%20Big%20Bend%20National%20Park%2C%20TX%2079834/today?unitGroup=us&key=GV95TV8AQBAEB4TB8G9UMEQEM&include=days&elements=datetime,moonphase,sunrise,sunset`;
 
 // fetch our info from the API
 fetch(url)
   .then((resp) => resp.json())
   .then(function (data) {
     // get date
+    console.log(data);
+    console.log(data.days[0].moonphase);
+    console.log(data.days[0].sunrise);
     const todaysDate = new Date(n);
-
+    let moonphase = data.days[0].moonphase;
+    let currentSunrise = data.days[0].sunrise;
+    let currentSunset = data.days[0].sunset;
     // reformat date
+
     const longEnUSFormatter = new Intl.DateTimeFormat("en-US", {
       year: "numeric",
       month: "long",
@@ -26,49 +32,42 @@ fetch(url)
     document.getElementById("date").innerHTML =
       longEnUSFormatter.format(todaysDate);
 
+    // document.getElementById("location").innerHTML =
+    //   "Big Bend National Park";
+
     // Cases to select corresponding image
-    switch (data[0]["Phase"]) {
-      case "Full Moon":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/ful.jpg' />"
-        break;
-      case "New Moon":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/new.jpg />"
-        break;
-      case "Waning Gibbous":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/wng.jpg' />";
-        break;
-      case "Waxing Gibbous":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/wxg.jpg' />";
-        break;
-      case "Waning Crescent":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/wnc.jpg' />";
-        break;
-      case "Waxing Crescent":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/wxc.jpg' />";
-        break;
-      case "First Quarter":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/fqt.jpg' />";
-        break;
-      case "Third Quarter":
-        document.getElementById("moon-pix").innerHTML =
-          "<img src='assets/images/moon-phases-master/img/tqt.jpg' />";
-        break;
-      default:
-        break;
+
+    if ((moonphase == "0.5")) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/ful.jpg' />";
+    } else if ((moonphase === "0")) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/new.jpg />";
+    } else if (moonphase >= 0.49 || moonphase <= 0.75) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/wng.jpg' />";
+    } else if (moonphase >= 0.25 || moonphase <= 0.49) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/wxg.jpg' />";
+    } else if (moonphase >= 0.75 || moonphase <= 0.99) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/wnc.jpg' />";
+    } else if (moonphase >= 0.01 || moonphase <= 0.24) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/wxc.jpg' />";
+    } else if ((moonphase == 0.25)) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/fqt.jpg' />";
+    } else if ((moonphase == 0.75)) {
+      document.getElementById("moon-pix").innerHTML =
+        "<img src='assets/images/moon-phases-master/img/tqt.jpg' />";
     }
 
     // provide phase and illumination
-    document.getElementById("moon-phase").innerHTML = "Current Phase: " + data[0]["Phase"];
-    document.getElementById("moon-illumination").innerHTML =
-      "Current Illumination: " + data[0]["Illumination"].toFixed(2) * 100 + "%";
-  })
-  .catch(function (error) {
-    console.log(error);
+    document.getElementById("sunrise").innerHTML =
+      "Current Sunrise Time: " + currentSunrise;
+    document.getElementById("sunset").innerHTML =
+      "Current Sunset Time: " + currentSunset;
   });
+  
+
