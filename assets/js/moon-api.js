a3cc8393bced00a00bdedbf1fef3945debdce8b3
Jsummers1 <julie.summers2000@gmail.com>
2021-10-06 16:32:17 -0500

    finally works

--- assets/js/moon-api.js
+++ assets/js/moon-api.js
@@ -4,9 +4,6 @@ d = Date.now();
 
 const n = new Date(d) - 18000000;
 
-// format date for API to EPOCH
-dt = (n / 1000) | 0;
-
 const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/1%20Panther%20Junction%20Big%20Bend%20National%20Park%2C%20TX%2079834/today?unitGroup=us&key=GV95TV8AQBAEB4TB8G9UMEQEM&include=days&elements=datetime,moonphase,sunrise,sunset`;
 
 // fetch our info from the API
@@ -17,12 +14,42 @@ fetch(url)
     console.log(data);
     console.log(data.days[0].moonphase);
     console.log(data.days[0].sunrise);
+    console.log(data.resolvedAddress);  
+
     const todaysDate = new Date(n);
     let moonphase = data.days[0].moonphase;
     let currentSunrise = data.days[0].sunrise;
     let currentSunset = data.days[0].sunset;
+    let currentAddress= data.resolvedAddress;
 
-    console.log (typeof moonphase);
+    console.log(typeof moonphase);
+
+    // Cases to select corresponding image
+    var imageHolder = "";
+    if (moonphase === 0.5) {
+      imageHolder = "<img src='./assets/images/moon-phases-master/img/ful.jpg'/>";
+    } else if (moonphase === 0) {
+      imageHolder = "<img src='./assets/images/moon-phases-master/img/new.jpg'/>";
+    } else if (moonphase >= 0.49 && moonphase <= 0.75) {
+      imageHolder =
+        "<img src='./assets/images/moon-phases-master/img/wng.jpg'/>";
+    } else if (moonphase >= 0.25 && moonphase <= 0.49) {
+      imageHolder =
+        "<img src='./assets/images/moon-phases-master/img/wxg.jpg' />";
+    } else if (moonphase >= 0.75 && moonphase <= 1) {
+      imageHolder =
+        "<img src='./assets/images/moon-phases-master/img/wnc.jpg' />";
+    } else if (moonphase >= 0.01 && moonphase <= 0.24) {
+      imageHolder =
+        "<img src='./assets/images/moon-phases-master/img/wxc.jpg' />";
+    } else if (moonphase === 0.25) {
+      imageHolder =
+        "<img src='./assets/images/moon-phases-master/img/fqt.jpg' />";
+    } else if (moonphase === 0.75) {
+      imageHolder =
+        "<img src='./assets/images/moon-phases-master/img/tqt.jpg' />";
+    }
+
     // reformat date
 
     const longEnUSFormatter = new Intl.DateTimeFormat("en-US", {
@@ -31,40 +58,11 @@ fetch(url)
       day: "numeric",
     });
 
+   document.getElementById("addy").innerHTML = currentAddress;
     document.getElementById("date").innerHTML =
       longEnUSFormatter.format(todaysDate);
-
-    // document.getElementById("location").innerHTML ="Big Bend National Park";
-
-    // Cases to select corresponding image
-
-    if (moonphase === 0.5) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/ful.jpg' />";
-    } else if (moonphase === 0) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/new.jpg />";
-    } else if (moonphase >= 0.49 && moonphase <= 0.75) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/wng.jpg' />";
-    } else if (moonphase >= 0.25 && moonphase <= 0.49) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/wxg.jpg' />";
-    } else if (moonphase >= 0.75 && moonphase <= 0.99) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/wnc.jpg' />";
-    } else if (moonphase >= 0.01 && moonphase <= 0.24) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/wxc.jpg' />";
-    } else if (moonphase === 0.25) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/fqt.jpg' />";
-    } else if (moonphase === 0.75) {
-      document.getElementById("moon-pix").innerHTML =
-        "<img src='assets/images/moon-phases-master/img/tqt.jpg' />";
-    }
-
-    // provide phase and illumination
+   document.getElementById("moon-pix").innerHTML = imageHolder;
+    // provide sunrise and sunset times
     document.getElementById("sunrise").innerHTML =
       "Current Sunrise Time: " + currentSunrise;
     document.getElementById("sunset").innerHTML =
